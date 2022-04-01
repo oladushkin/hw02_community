4d36e87437ed0935e621ad6f824337c1014cfd26
Дима Шелепин <100335989+oladushkin@users.noreply.github.com>
2022-04-01 19:56:51 +0300

    Update admin.py

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import Group, Post
 
-# Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
@@ -16,7 +15,6 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
-
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
