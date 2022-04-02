f74c53fda4d6ab8451829e541803c9d1774a5b25
Дима Шелепин <100335989+oladushkin@users.noreply.github.com>
2022-04-02 12:53:02 +0300

    загружено вручную

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Group, Post
 
+# Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,22 +1,30 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-
-# Create your models here.
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=20, unique=True, blank=False)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы',
+        help_text='Название недолжно привышать 200 символов.'
+    )
+    slug = models.SlugField(
+        max_length=20,
+        unique=True,
+        blank=False,
+        verbose_name='Slug вашей группы',
+        help_text='Пишите на английском языке'
+    )
+    description = models.TextField(verbose_name='Описание группы')
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name='Текст сообщения')
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
@@ -25,7 +33,11 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
-        null=True
+        null=True,
+        related_name='group'
     )
+
+    class Meta:
+        ordering = ('-pub_date')
